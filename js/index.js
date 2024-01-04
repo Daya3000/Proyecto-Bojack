b74ec1ce2a16f42699ebfa053c8c00815f9f9746
Daya3000 <154549092+Daya3000@users.noreply.github.com>
2024-01-04 22:09:47 +0100

    cambios css

--- js/index.js
+++ js/index.js
@@ -20,24 +20,58 @@ botones.forEach( boton => boton.addEventListener("click" , () => modalMenu.class
 // console.log(personajes)
 
 
-const botonBtn = document.querySelector('.boton__button')
-const personajesUl = document.querySelector('.personajes__ul')
-const personajesIl = document.querySelectorAll('.personas__il')
+// const botonBtn = document.querySelector('.boton__button')
+// const personajesUl = document.querySelector('.personajes__ul')
+// const personajesIl = document.querySelectorAll('.personas__il')
 
-console.log(botonBtn)
-console.log(personajesUl)
+// console.log(botonBtn)
+// console.log(personajesUl)
 
-botonBtn.addEventListener('click', function(){
+// botonBtn.addEventListener('click', function(){
     
     
 
-    personajesIl.forEach( function (eachIl , index){
-        personajesUl[index].addEventListener('click', function(){
+//     personajesIl.forEach( function (eachIl , index){
+//         personajesUl[index].addEventListener('click', function(){
 
+//         })
+//         let calculo = personajesIl * (100 / 4 )
+//         console.log(calculo)
+//     })
+//     personajesIl.style.transform = 'translateX(-${calculo}%)'
+// })
+
+
+const botonesFilosofos = document.querySelectorAll('.filosofos__h2')
+const frasesFilosofos = document.querySelectorAll('.filosofos__p')
+
+console.log(botonesFilosofos)
+console.log(frasesFilosofos)
+
+botonesFilosofos.forEach( function ( eachFilosofo , index ){
+
+    botonesFilosofos[index].addEventListener( 'click' , function (){
+        frasesFilosofos.forEach( function ( eachFrase , index ){
+            frasesFilosofos[index].classList.remove('isHover')
+            
         })
-        let calculo = personajesIl * (100 / 4 )
-        console.log(calculo)
+        frasesFilosofos[index].classList.add('isHover')
     })
-    personajesIl.style.transform = 'translateX(-${calculo}%)'
 })
-   
+
+const leccionesBarra = document.querySelectorAll('.lecciones')
+const lecciones = document.querySelectorAll('.leccion')
+const reflexiones = document.querySelectorAll('.reflexion')
+
+console.log(leccionesBarra)
+console.log(reflexiones)
+console.log(lecciones)
+
+leccionesBarra.forEach( function( eachBarra , index ){
+    leccionesBarra[index].classList.remove('active')
+    leccionesBarra[index].addEventListener( 'click' , function (){
+        lecciones[index].classList.toggle('active')
+        leccionesBarra[index].classList.toggle('active')
+        reflexiones[index].classList.toggle('active')
+    })
+})
