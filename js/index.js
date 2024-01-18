28ea33dc0576c596fcec29d26fe4ba28688b8795
Daya3000 <154549092+Daya3000@users.noreply.github.com>
2024-01-18 17:18:46 +0100

    responsive pag

--- js/index.js
+++ js/index.js
@@ -5,16 +5,40 @@ const modalMenu = document.querySelector(".modal__menu");
 const botones = document.querySelectorAll(".abrir__svg , .cerrar__svg");
 const todoBody = document.querySelector("body")
 
-// botones.forEach((boton) =>
-  
-//   boton.addEventListener("click", () => modalMenu.classList.toggle("active"))
-// );
-
 botones.forEach( function ( eachBoton , index ){
   eachBoton.addEventListener("click" , function() {
     modalMenu.classList.toggle("active")
     todoBody.classList.toggle("mostrar")
   })
+})
+
+const pastillas = document.querySelectorAll(".caja__span")
+
+pastillas.forEach( function ( eachPastilla , index ){
+  pastillas[index].addEventListener("click" , function(){
+
+    const colorRandom = `rgb(${Math.ceil(Math.random() * 255)}, ${Math.ceil(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
+    
+    eachPastilla.style.backgroundColor = colorRandom
+  } )
+})
+
+const cardMujeres = document.querySelectorAll(".mujeres__lista")
+const modalLightbox = document.querySelector(".lightbox")
+const imgLightbox = document.querySelectorAll(".imgActiva")
+const botonCerrar = document.querySelector(".mujeres__button")
+
+cardMujeres.forEach( function ( eachCard , index ){
+  cardMujeres[index].addEventListener("click" , function(){
+    modalLightbox.classList.add("modal")
+    imgLightbox.forEach( function ( eachImg , index ){
+      imgLightbox[index].classList.remove("modal")
+    })
+    imgLightbox[index].classList.add("modal")
+  })
+})
+botonCerrar && botonCerrar.addEventListener("click" , function(){
+  modalLightbox.classList.remove("modal")
 })
 
 
@@ -24,8 +48,6 @@ botones.forEach( function ( eachBoton , index ){
 const botonesFilosofos = document.querySelectorAll(".filosofos__h3");
 const frasesFilosofos = document.querySelectorAll(".filosofos__p");
 
-console.log(botonesFilosofos);
-console.log(frasesFilosofos);
 
 botonesFilosofos.forEach(function (eachFilosofo, index) {
   botonesFilosofos[index].addEventListener("click", function () {
@@ -44,9 +66,6 @@ const leccionesBarra = document.querySelectorAll(".lecciones");
 const lecciones = document.querySelectorAll(".leccion");
 const reflexiones = document.querySelectorAll(".reflexion");
 
-console.log(leccionesBarra);
-console.log(reflexiones);
-console.log(lecciones);
 
 leccionesBarra.forEach(function (eachBarra, index) {
   leccionesBarra[index].classList.remove("active");
@@ -56,3 +75,25 @@ leccionesBarra.forEach(function (eachBarra, index) {
     reflexiones[index].classList.toggle("active");
   });
 });
+
+
+
+const sliderContenedor = document.querySelector(".slider__contenedor")
+const flechas = document.querySelectorAll(".slider__flecha")
+const marcadores = document.querySelectorAll(".slider__btn")
+console.log(marcadores)
+
+marcadores.forEach(function( eachMarcador , index ){
+  
+  marcadores[index].addEventListener("click" , function(){
+    let calculo = index * (100 / 5)
+    console.log(calculo)
+    sliderContenedor.style.transform = `translateX(-${calculo}%)`
+
+    marcadores.forEach(function ( eachMarcador , index){
+      marcadores[index].classList.remove("pintado")
+    })
+    marcadores[index].classList.add("pintado")
+
+  })
+})
